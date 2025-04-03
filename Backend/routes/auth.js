7af8c75e2543711a7f9811154bee808bcde106c4
kevin <kevin.frazier059@gmail.com>
2025-04-03 02:27:34 -0400

    backend routes for login page

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,9 +1,11 @@
 // routes for user authentication (signup, login, logout) using JWT.
 import express from "express";
 import { register } from "../controllers/auth.js";
+import { login } from "../controllers/auth.js"; // Import from login
 
 const router = express.Router();
 
 router.post("/register", register)
+router.post("/login", login)
 
 export default router;
--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,10 +1,30 @@
 //Manages user profile actions, such as retrieving or updating profile information.
 import express from "express";
+import {updateUser, deleteUser,getUser, getUsers} from "../controllers/user.js";
+import { verifyAdmin, verifyToken,verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Hello thi is auth endpoint");
-});
+router.get("/checkauthentication", verifyToken, (req,res,next)=>{
+    res.send("Hello user you are logged in")
+})
+router.get("/checkuser/:id",verifyUser, (req,res,next)=>{
+    res.send("Hello user you are logged in and can delete your account")
+})
+router.get("/checkadmin/:id",verifyAdmin, (req,res,next)=>{
+    res.send("Hello user you are logged in and can delete all accounts")
+})
+//UPDATE
+router.put("/:id", verifyUser, updateUser);
+
+//DELETE
+router.delete("/:id", verifyUser, deleteUser);
+
+//GET
+router.get("/:id", verifyUser, getUser);
+
+//GET ALL
+router.get("/",verifyAdmin, getUsers);
+
 
 export default router;
