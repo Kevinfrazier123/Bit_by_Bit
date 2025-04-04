a05038371b886df0add618f9e4994450eaa4fca7
kevin <kevin.frazier059@gmail.com>
2025-04-04 03:23:57 -0400

    frontend set up and login page complete

---
+++ Frontend/src/pages/home/Home.jsx
@@ -0,0 +1,22 @@
+
+
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div>
+     
+      <div className="homeContainer">
+       
+        <h1 className="homeTitle"> This is the Homepage</h1>
+        
+        <h1 className="homeTitle">Hello world</h1>
+       
+       
+     
+      </div>
+    </div>
+  );
+};
+
+export default Home;
